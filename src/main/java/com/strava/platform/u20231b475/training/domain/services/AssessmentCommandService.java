736f63d23641482f6e13f7e37c4a064a07c8819f
Trevor <valentinosolischang@gmail.com>
2025-11-20 10:52:30 -0500

    Enhance AssessmentCommandService interface documentation for clarity

--- src/main/java/com/strava/platform/u20231b475/training/domain/services/AssessmentCommandService.java
+++ src/main/java/com/strava/platform/u20231b475/training/domain/services/AssessmentCommandService.java
@@ -5,6 +5,17 @@ import java.util.Optional;
 import com.strava.platform.u20231b475.training.domain.model.aggregates.Assessment;
 import com.strava.platform.u20231b475.training.domain.model.commands.CreateAssessmentCommand;
 
+/**
+ * Assessment Command Service
+ */
 public interface AssessmentCommandService {
+  /**
+   * Handle create an Assessment Command
+   * 
+   * @param command The {@link CreateAssessmentCommand} command
+   * @return A {@link Assessment} instance if the command is valid, otherwise
+   *         false
+   * @throws IllegalArgumentException if the assessment already exists
+   */
   Optional<Assessment> handle(CreateAssessmentCommand command);
 }
