72485cd03289b21d37588681b6eae1182375eb05
Trevor <valentinosolischang@gmail.com>
2025-11-20 10:57:36 -0500

    Enhance AssessmentQueryService documentation for clarity

--- src/main/java/com/strava/platform/u20231b475/training/domain/services/AssessmentQueryService.java
+++ src/main/java/com/strava/platform/u20231b475/training/domain/services/AssessmentQueryService.java
@@ -5,6 +5,15 @@ import java.util.Optional;
 import com.strava.platform.u20231b475.training.domain.model.aggregates.Assessment;
 import com.strava.platform.u20231b475.training.domain.model.queries.GetAssessmentByAthleteIdAndCoachIdAndDateQuery;
 
+/**
+ * Assessment Query Service
+ */
 public interface AssessmentQueryService {
+  /**
+   * Handle get Assessment by athleteId, coachId and the actual date
+   * 
+   * @param query The {@link GetAssessmentByAthleteIdAndCoachIdAndDateQuery} query
+   * @return A {@link Assessment} instance if the query is valid, otherwise empty
+   */
   Optional<Assessment> handle(GetAssessmentByAthleteIdAndCoachIdAndDateQuery query);
 }
