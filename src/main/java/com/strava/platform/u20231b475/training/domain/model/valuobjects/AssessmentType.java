b2850172585816eca14facd7f971610f1d527012
Trevor <valentinosolischang@gmail.com>
2025-11-19 23:42:44 -0500

    Add AssessmentType enum for valid assessment types

---
+++ src/main/java/com/strava/platform/u20231b475/training/domain/model/valuobjects/AssessmentType.java
@@ -0,0 +1,12 @@
+package com.strava.platform.u20231b475.training.domain.model.valuobjects;
+
+/**
+ * Enum for the valid assessment types
+ * 
+ * @author Valentino Solis
+ */
+public enum AssessmentType {
+  STRENGTH,
+  CARDIO,
+  RECOVERY
+}
