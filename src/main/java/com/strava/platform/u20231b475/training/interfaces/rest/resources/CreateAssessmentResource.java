a29000bd2f40408e5048c91f1793059d9cf63a5c
Trevor <valentinosolischang@gmail.com>
2025-11-20 11:08:44 -0500

    Add CreateAssessmentResource for assessment creation

---
+++ src/main/java/com/strava/platform/u20231b475/training/interfaces/rest/resources/CreateAssessmentResource.java
@@ -0,0 +1,22 @@
+package com.strava.platform.u20231b475.training.interfaces.rest.resources;
+
+import java.math.BigDecimal;
+
+import com.strava.platform.u20231b475.training.domain.model.valuobjects.AssessmentType;
+
+import jakarta.validation.constraints.*;
+
+/**
+ * Resource for creating an assessment
+ */
+public record CreateAssessmentResource(@NotNull Long athleteId,
+    @NotNull Long coachId,
+    @NotNull @DecimalMin("10.0") @DecimalMax("40.0") BigDecimal bmi,
+    @NotNull @Min(0) @Max(200) Integer pushUps,
+    @NotNull @Min(0) @Max(600) Integer plankTime,
+    @NotNull @Min(80) @Max(220) Integer maxHeartRate,
+    @NotNull @Min(30) @Max(120) Integer restingHeartRate,
+    @NotNull @Min(20) @Max(80) Integer vo2max,
+    @NotBlank String encryptedText,
+    @NotNull AssessmentType type) {
+}
