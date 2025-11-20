d81d41ff14e8115a3e6d696681dba363f96feba1
Trevor <valentinosolischang@gmail.com>
2025-11-20 12:23:49 -0500

    Format CreateAssessmentCommand parameters for improved readability

--- src/main/java/com/strava/platform/u20231b475/training/domain/model/commands/CreateAssessmentCommand.java
+++ src/main/java/com/strava/platform/u20231b475/training/domain/model/commands/CreateAssessmentCommand.java
@@ -10,5 +10,6 @@ import com.strava.platform.u20231b475.training.domain.model.valuobjects.Assessme
  * @author Valentino Solis
  */
 public record CreateAssessmentCommand(Long athleteId, Long coachId, BigDecimal bmi, Integer pushUps, Integer plankTime,
-    Integer maxHeartRate, Integer restingHeartRate, Integer vo2max, String encryptedText, AssessmentType type) {
+                Integer maxHeartRate, Integer restingHeartRate, Integer vo2max, String encryptedText,
+                AssessmentType type) {
 }
